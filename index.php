98933d72e8804c31b3bd0844bd4c53c8cba08790
umarrik <82759611+umarrik@users.noreply.github.com>
2021-04-19 04:53:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+$pdo = new PDO('pgsql:host=localhost;dbname=messages', 'kali', 'kali##');
+
+$date = date('Y-m-d_h:i:s A');
+
+
+if (isset($_POST['message']) AND $_POST['message'] != "") {
+  $query = "INSERT INTO messages (message, created) VALUES ('".$_POST['message']."' , '".$date."')";
+  $pdo->query($query);
+}
+
+if (isset($_POST['command']) AND $_POST['command'] == 'clear') {
+  $query = "TRUNCATE messages";
+  $pdo->query($query);
+}
+
+$query = $pdo->query('SELECT * FROM messages');
+$res = $query->fetchAll();
+//print_r($res[0]);
+
+
+function cmp($a, $b)
+{
+
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a > $b) ? -1 : 1;
+}
+$mes = array();
+foreach ($res as $key => $val) {
+  $mes[$key]['id'] = $val['id'];
+  $mes[$key]['message'] = $val['message'];
+  $mes[$key]['created'] = $val['created'];
+}
+usort($mes, "cmp");
+
+ ?>
+ <!DOCTYPE HTML>
+ <html>
+  <head>
+   <meta charset="utf-8">
+   <title>385_1</title>
+  </head>
+  <body>
+
+    <?php foreach ($mes as $key => $val):?>
+    <p><b><?php print_r($val['message']."  ".$val['created']); ?><br></b></p>
+    <?php endforeach;?>
+
+
+  <form action="index.php" method="post">
+   <p><Br>
+   <input type="text" name="message" value=""></p>
+   <p><input type="submit"></p>
+  </form>
+
+
+  <form action="index.php" method="post">
+   <p><Br>
+   <input type="hidden" name="command" value="clear"></p>
+   <p><input type="submit" value="удалить все сообщения"></p>
+  </form>
+  </body>
+ </html>
